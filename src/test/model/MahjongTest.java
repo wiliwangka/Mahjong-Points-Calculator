1050578cfc3e4c0efaf168937d32fb7a875c63b9
williamwang <williamwang921220@gmail.com>
2022-10-25 22:12:42 -0700

    add seven pairs yaku

--- src/test/model/MahjongTest.java
+++ src/test/model/MahjongTest.java
@@ -67,6 +67,7 @@ class MahjongTest {
      assertTrue(2==game.getHand().get(1).getIdNum() && game.getHand().get(1).getCatergory().equals("So"));
      assertEquals (1, game.getHand().get(2).getCount());
         assertTrue(3==game.getHand().get(1).getIdNum() && game.getHand().get(1).getCatergory().equals("Man"));
+        assertEquals(9,game.getHand().size());
 
     }
 @Test
